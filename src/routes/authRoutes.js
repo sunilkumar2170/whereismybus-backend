802ec26d5f12a52847b1311b372b68757339c02a
sunilkumar2170 <sunilkr@iitbhilai.ac.in>
2026-07-12 13:44:23 +0530

    Admin full backend added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register, login, adminLogin } = require('../controllers/authController');
 
-router.post('/register', register);s
+router.post('/register', register);
 router.post('/login', login);
 router.post('/admin-login', adminLogin);
 
